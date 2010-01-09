3322a6fe7df0c88c4bf52a5c6bf46cad5f98705b
callahat <tim.callahan25@yahoo.com>
2010-01-09 19:46:06 +0000

    Rename the timestamp columns instead of creating a new column, copying over the value, then dropping the old.

--- db/migrate/002_auto_timestamps.rb
+++ db/migrate/002_auto_timestamps.rb
@@ -1,31 +1,13 @@
 class AutoTimestamps < ActiveRecord::Migration
   def self.up
-    add_column :grammars, :created_at, :datetime
-	add_column :grammars, :updated_at, :datetime
-	add_column :users,    :created_at, :datetime
-	
-	Grammar.find(:all).each{|g|
-	  g.update_attributes(:created_at => g.created, :updated_at => g.modified)}
-	User.find(:all).each{|u|
-	  u.update_attributes(:created_at => u.timestamp)}
-	
-    remove_column :grammars, :created
-	remove_column :grammars, :modified
-	remove_column :users,    :timestamp
+    rename_column :grammars, :created,   :created_at
+	rename_column :grammars, :modified,  :updated_at
+	rename_column :users,    :timestamp, :created_at
   end
 
   def self.down
-    add_column :grammars, :created,   :datetime
-	add_column :grammars, :modified,  :datetime
-	add_column :users,    :timestamp, :datetime
-	
-	Grammar.find(:all).each{|g|
-	  g.update_attributes(:created => g.created_at, :modified => g.updated_at)}
-	User.find(:all).each{|u|
-	  u.update_attributes(:timestamp => u.created_at)}
-	
-    remove_column :grammars, :created_at
-	remove_column :grammars, :updated_at
-	remove_column :users,    :created_at
+    rename_column :grammars, :created_at, :created
+	rename_column :grammars, :updated_at, :modified
+	rename_column :users,    :created_at, :timestamp
   end
 end
